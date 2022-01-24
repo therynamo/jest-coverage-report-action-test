77fb50d3727e282a6b7fb319e9ae251a8345ef76
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-01-24 20:31:27 +0200

    Threshold removed

--- project/jest.config.js
+++ project/jest.config.js
@@ -1,16 +1,5 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  collectCoverageFrom: ["**/*.ts"],
-  coverageThreshold: {
-    global: {
-      functions: -1,
-    },
-    "*l.ts": {
-      statements: 51,
-    },
-    "a.ts": {
-      functions: 100,
-    },
-  },
+    preset: "ts-jest",
+    testEnvironment: "node",
+    collectCoverageFrom: ["**/*.ts"],
 };
