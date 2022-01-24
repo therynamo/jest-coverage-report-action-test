7347effa10caf3968ff47c9f8bbdb05a6542f8c9
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-01-24 20:58:15 +0200

    ttt

--- project/jest.config.js
+++ project/jest.config.js
@@ -2,4 +2,15 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     collectCoverageFrom: ["**/*.ts"],
+    coverageThreshold: {
+        global: {
+            functions: -1,
+        },
+        "*l.ts": {
+            statements: 51,
+        },
+        "a.ts": {
+            functions: 100,
+        },
+    },
 };
